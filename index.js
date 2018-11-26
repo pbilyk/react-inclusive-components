55a403cc5991bb8beec838a4da21220b2bc96062
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2018-11-26 10:56:29 +0200

    Add the build feature
    Modified dependencies
    
    1.0.2
    
    1.0.3
    
    add npm ignore
    
    Updated peer dependencies

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-import Heading from "./components/Heading";
-import Section from "./components/Section";
-import List from "./components/List";
-import Label from "./components/Label";
-import Input from "./components/Input";
-import LiveArea from "./components/LiveArea";
-import Fieldset from "./components/Fieldset";
-import Button from "./components/Button";
+import * as Components from './lib'
+
+const { Heading, Section, List, Label, Input, LiveArea, Fieldset, Button } = Components;
 
 export { Heading, Section, List, Label, Input, LiveArea, Fieldset, Button };
+
