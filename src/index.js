ac8f97943495e9bfa578c64a7b43477d2b6ff65e
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2018-11-26 10:56:29 +0200

    Add the build feature

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import List from "./components/List";
+import Label from "./components/Label";
+import Input from "./components/Input";
+import LiveArea from "./components/LiveArea";
+import Fieldset from "./components/Fieldset";
+import Button from "./components/Button";
+
+export { Heading, Section, List, Label, Input, LiveArea, Fieldset, Button };
