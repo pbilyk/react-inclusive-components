88e7ee31ce6ff41103ec6595a391b8621b71950f
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2018-11-26 12:24:31 +0200

    Fix the Section level defaultProp to be string

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import Base from  "./Base";
+import Base from "./Base";
 import Heading from "./Heading";
 
 const Section = props => {
-    const { level, label, labelId, noHeading } = props;
+  const { level, label, labelId, noHeading } = props;
 
-    const Section = styled(Base.withComponent('section'))``;
+  const Section = styled(Base.withComponent("section"))``;
 
-    return (
-        <Section aria-labelledby={labelId} {...props}>
-            <Heading id={labelId} element={`h${level}`} hidden={noHeading}>
-                {label}
-            </Heading>
-            {props.children}
-        </Section>
-    );
+  return (
+    <Section aria-labelledby={labelId} {...props}>
+      <Heading id={labelId} element={`h${level}`} hidden={noHeading}>
+        {label}
+      </Heading>
+      {props.children}
+    </Section>
+  );
 };
 
 Section.propTypes = {
-    label: PropTypes.string.isRequired,
-    labelId: PropTypes.string.isRequired,
-    level: PropTypes.string,
-    noHeading: PropTypes.bool
+  label: PropTypes.string.isRequired,
+  labelId: PropTypes.string.isRequired,
+  level: PropTypes.string,
+  noHeading: PropTypes.bool
 };
 
 Section.defaultProps = {
-    level: 1,
-    noHeading: false,
-}
+  level: "1",
+  noHeading: false
+};
 
 export default Section;
