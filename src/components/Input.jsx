e67f6d02a47efbce13fb23f7f998a686ba0a3941
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2018-11-28 11:51:03 +0200

    Fix for custom inputs behavior

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
-import React, {Fragment} from "react";
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import Base from "./Base";
 import Label from "./Label";
@@ -23,52 +23,93 @@ const InputView = styled(Base.withComponent("input"))`
   }
 `;
 
-const Input = (props) => {
-    const {
-        label,
-        id,
-        labelHidden,
-        custom,
-        customClassName,
-        labelClassName,
-        labelPosition
-    } = props;
+const Input = props => {
+  const {
+    label,
+    id,
+    labelHidden,
+    custom,
+    customClassName,
+    labelClassName,
+    labelPosition
+  } = props;
 
-    return (
-        <Fragment>
-            {labelPosition === 'before' && (
-                <Label className={`${labelClassName ? labelClassName : ''}`} htmlFor={id} hidden={labelHidden}>
-                    {label}
-                </Label>
-            )}
-            {!custom && <InputView {...props} />}
-            {custom && <Fragment><InputView hidden {...props} /><span className={`input-custom-element ${customClassName ? customClassName : ''}`} /></Fragment>}
-            {labelPosition === 'after' && (
-                <Label className={`${labelClassName ? labelClassName : ''}`} htmlFor={id} hidden={labelHidden}>
-                    {label}
-                </Label>
-            )}
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      {!custom &&
+        labelPosition === "before" && (
+          <Label
+            className={`${labelClassName ? labelClassName : ""}`}
+            htmlFor={id}
+            hidden={labelHidden}
+          >
+            {label}
+          </Label>
+        )}
+      {!custom && <InputView {...props} />}
+      {!custom &&
+        labelPosition === "after" && (
+          <Label
+            className={`${labelClassName ? labelClassName : ""}`}
+            htmlFor={id}
+            hidden={labelHidden}
+          >
+            {label}
+          </Label>
+        )}
+      {custom &&
+        labelPosition === "after" && (
+          <Label
+            className={`${labelClassName ? labelClassName : ""}`}
+            htmlFor={id}
+            hidden={labelHidden}
+          >
+            <InputView hidden {...props} />
+            <span
+              className={`input-custom-element ${
+                customClassName ? customClassName : ""
+              }`}
+            />
+            {label}
+          </Label>
+        )}
+      {custom &&
+        labelPosition === "before" && (
+          <Label
+            className={`${labelClassName ? labelClassName : ""}`}
+            htmlFor={id}
+            hidden={labelHidden}
+          >
+            <InputView hidden {...props} />
+            {label}
+            <span
+              className={`input-custom-element ${
+                customClassName ? customClassName : ""
+              }`}
+            />
+          </Label>
+        )}
+    </Fragment>
+  );
+};
 
 Input.propTypes = {
-    label: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    type: PropTypes.string,
-    labelPosition: PropTypes.string,
-    custom: PropTypes.bool,
-    customClassName: PropTypes.string,
-    labelClassName: PropTypes.string,
-    labelHidden: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  labelPosition: PropTypes.string,
+  custom: PropTypes.bool,
+  customClassName: PropTypes.string,
+  labelClassName: PropTypes.string,
+  labelHidden: PropTypes.bool
 };
 
 Input.defaultProps = {
-    placeholder: "",
-    type: "text",
-    labelPosition: "before",
-    custom: false,
-}
+  placeholder: "",
+  type: "text",
+  labelPosition: "before",
+  custom: false
+};
 
 export default Input;
