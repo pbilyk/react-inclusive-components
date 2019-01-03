d0a2e126eefbc016b4fa2b71d19e9db5d7cd38d9
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2019-01-03 13:20:46 +0200

    Transpile index.js to work with nextjs

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import * as Components from './lib'
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.Button = exports.Fieldset = exports.LiveArea = exports.Input = exports.Label = exports.List = exports.Section = exports.Heading = undefined;
 
-const { Heading, Section, List, Label, Input, LiveArea, Fieldset, Button } = Components;
+var _lib = require('./lib');
 
-export { Heading, Section, List, Label, Input, LiveArea, Fieldset, Button };
+var Components = _interopRequireWildcard(_lib);
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+var Heading = Components.Heading,
+    Section = Components.Section,
+    List = Components.List,
+    Label = Components.Label,
+    Input = Components.Input,
+    LiveArea = Components.LiveArea,
+    Fieldset = Components.Fieldset,
+    Button = Components.Button;
+
+exports.Heading = Heading;
+exports.Section = Section;
+exports.List = List;
+exports.Label = Label;
+exports.Input = Input;
+exports.LiveArea = LiveArea;
+exports.Fieldset = Fieldset;
+exports.Button = Button;
