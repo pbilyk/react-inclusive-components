53c14602a655a941908c4b37c271da1e7fb34d2a
Pavlo Bilyk <pavlo.bilyk@innocode.no>
2019-01-28 18:36:07 +0200

    Expose TextArea

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
+import TextArea from "./src/components/TextArea";
+
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.Button = exports.Fieldset = exports.LiveArea = exports.Input = exports.Label = exports.List = exports.Section = exports.Heading = undefined;
 
-var _lib = require('./lib');
+var _lib = require("./lib");
 
 var Components = _interopRequireWildcard(_lib);
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};
+    if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key))
+          newObj[key] = obj[key];
+      }
+    }
+    newObj.default = obj;
+    return newObj;
+  }
+}
 
 var Heading = Components.Heading,
-    Section = Components.Section,
-    List = Components.List,
-    Label = Components.Label,
-    Input = Components.Input,
-    LiveArea = Components.LiveArea,
-    Fieldset = Components.Fieldset,
-    Button = Components.Button;
+  Section = Components.Section,
+  List = Components.List,
+  Label = Components.Label,
+  Input = Components.Input,
+  LiveArea = Components.LiveArea,
+  Fieldset = Components.Fieldset,
+  Button = Components.Button;
+TextArea = Components.TextArea;
 
 exports.Heading = Heading;
 exports.Section = Section;
@@ -26,3 +43,4 @@ exports.Input = Input;
 exports.LiveArea = LiveArea;
 exports.Fieldset = Fieldset;
 exports.Button = Button;
+exports.TextArea = TextArea;
